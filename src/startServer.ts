2cc628ec64c7eb4d54e73621bd73ff6dca981ec5
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-11-30 20:54:38 +0530

    chore: use custom function to tackle cors

--- src/startServer.ts
+++ src/startServer.ts
@@ -20,12 +20,27 @@ export default async function () {
 			: "http://localhost:8080";
 
 	app.disable("x-powered-by");
-	app.use(
-		cors({
-			origin,
-			credentials: true
-		})
-	);
+	app.use(function (req, res, next) {
+		res.header("Access-Control-Allow-Origin", "*");
+		res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+		res.header(
+			"Access-Control-Allow-Headers",
+			"Content-Type, Authorization, Content-Length, X-Requested-With"
+		);
+
+		// intercept OPTIONS method
+		if ("OPTIONS" == req.method) {
+			res.send(200);
+		} else {
+			next();
+		}
+	});
+	// app.use(
+	// 	cors({
+	// 		origin,
+	// 		credentials: true
+	// 	})
+	// );
 	app.use(session);
 	app.use("/", routes);
 
