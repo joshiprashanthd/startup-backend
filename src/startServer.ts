5cdac689700be2188cdb9fae9202fb844e8cbb55
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-11-30 22:01:24 +0530

    chore: remove custom middleware

--- src/startServer.ts
+++ src/startServer.ts
@@ -20,21 +20,6 @@ export default async function () {
 			: "http://localhost:8080";
 
 	app.disable("x-powered-by");
-	app.use(function (req, res, next) {
-		res.header("Access-Control-Allow-Origin", "*");
-		res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-		res.header(
-			"Access-Control-Allow-Headers",
-			"Content-Type, Authorization, Content-Length, X-Requested-With"
-		);
-
-		// intercept OPTIONS method
-		if ("OPTIONS" == req.method) {
-			res.send(200);
-		} else {
-			next();
-		}
-	});
 	app.use(
 		cors({
 			origin,
